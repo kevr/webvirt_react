600c453ef5066d699a428df014df6f0a5402f28f
Kevin Morris <kevr@0cost.org>
2023-01-26 03:17:59 -0800

    feat: use localStorage cache of Redux session for its initial data
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- src/store/Reducers.js
+++ src/store/Reducers.js
@@ -5,8 +5,12 @@ const defaultState = {};
 export const sessionReducer = (state = defaultState, action) => {
   switch (action.type) {
     case SET_SESSION:
-      return Object.assign({}, state, action.session, { authenticated: true });
+      const updatedState = Object.assign({}, state, action.session, {
+        authenticated: true,
+      });
+      localStorage.setItem("session", updatedState);
     case REMOVE_SESSION:
+      localStorage.removeItem("session");
       return defaultState;
     default:
       return state;
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { sessionReducer } from "./Reducers";
 
-export const createStore = () =>
-  configureStore({
-    reducer: {
-      session: sessionReducer,
+export const createStore = () => {
+  const localSession = localStorage.getItem("session") || {};
+
+  const initialState = {
+    session: localSession,
+  };
+
+  return configureStore(
+    {
+      reducer: {
+        session: sessionReducer,
+      },
     },
-  });
+    initialState
+  );
+};
