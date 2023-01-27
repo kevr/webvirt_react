36ffdf984baa54f1f0f73c2ab2d975f8fdbb67f1
Kevin Morris <kevr@0cost.org>
2023-01-27 01:04:34 -0800

    feat: add more <DomainCard> colors
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- src/components/DomainCard.js
+++ src/components/DomainCard.js
@@ -2,20 +2,32 @@ import { Tooltip } from "react-tooltip";
 
 const STATE_COLORS = {
   None: "",
-  Running: "green-text text-darken-2",
-  Blocked: "red-text text-darken-2",
-  Paused: "grey-text text-darken-2",
-  Shutdown: "orange-text",
+  Running: "green-text text-darken-4",
+  Blocked: "red-text text-darken-4",
+  Paused: "grey-text text-darken-3",
+  Shutdown: "grey-text text-darken-3",
   Shutoff: "",
-  Crashed: "red-text text-darken-2",
-  Suspended: "grey-text text-darken-2",
+  Crashed: "red-text text-darken-4",
+  Suspended: "blue-text text-darken-4",
+};
+
+const STATE_CARD_COLORS = {
+  None: "",
+  Running: "green lighten-5",
+  Blocked: "red lighten-4",
+  Paused: "grey lighten-1",
+  Shutdown: "yellow lighten-5",
+  Shutoff: "grey lighten-4",
+  Crashed: "red lighten-4",
+  Suspended: "blue lighten-5",
 };
 
 const DomainCard = ({ domain, uuid }) => {
   const color = STATE_COLORS[domain.state.string];
+  const cardColor = STATE_CARD_COLORS[domain.state.string];
   return (
     <div className="col s6 m4">
-      <div className="card">
+      <div className={`card ${cardColor}`}>
         <div
           id={`domain-tooltip-${uuid}`}
           data-tooltip-content={`${domain.name}: ${domain.state.string}`}
