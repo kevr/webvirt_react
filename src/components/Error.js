867da976b8178930f22760f4c185098f9e5a964c
Kevin Morris <kevr@0cost.org>
2023-01-28 17:09:11 -0800

    change(Error): support inline, enabled props; use new error data format
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,16 +1,22 @@
-const Error = ({ error }) => {
-  if (!error) {
+const Error = ({ inline, enabled, error }) => {
+  if (!enabled) {
     return <span />;
   }
 
   return (
     <div className="red-text text-lighten-1 text-center">
-      <div>
-        <i className="material-icons medium">error</i>
-      </div>
-      <div className="error">{error}</div>
+      {!inline && (
+        <div>
+          <i className="material-icons medium">error</i>
+        </div>
+      )}
+      <div className="error">{error.data.detail}</div>
     </div>
   );
+};
+
+Error.defaultProps = {
+  inline: false,
 };
 
 export default Error;
