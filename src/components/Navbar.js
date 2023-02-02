6bcb0664dd1e4fb3cab1296b3cfb87955170de85
Kevin Morris <kevr@0cost.org>
2023-02-02 02:56:49 -0800

    feat(Navbar): include title and nav item toward the homepage
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,26 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { Tooltip } from "react-tooltip";
 import Session from "./Session";
 import SessionReducer from "./SessionRefresher";
 
 const Navbar = () => {
+  const title = useSelector((state) => state.app.title);
+
   return (
     <nav>
       <div className="nav-wrapper">
         <div className="container flex-display flex-row">
+          <ul>
+            <li>
+              <Link id="dashboard-link" data-tooltip-content="Dashboard" to="/">
+                <i className="material-icons">home</i>
+              </Link>
+            </li>
+          </ul>
+          <div className="brand-logo-wrapper overflow-ellipsis">
+            <span className="title text-center">{title}</span>
+          </div>
           <div className="flex"></div>
           <div className="auth-component-wrapper">
             <SessionReducer>
@@ -14,6 +29,7 @@ const Navbar = () => {
           </div>
         </div>
       </div>
+      <Tooltip anchorId="dashboard-link" place="bottom" />
     </nav>
   );
 };
